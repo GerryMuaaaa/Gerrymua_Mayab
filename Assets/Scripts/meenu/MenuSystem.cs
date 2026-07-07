71f7f7c4e9cfe3dbdac25727a77ac2972da64063
GerryMuaaaa <dg.8159309@gmail.com>
2026-07-07 12:26:44 -0600

    Subiendo proyecto base de Unity

---
+++ Assets/Scripts/meenu/MenuSystem.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuSystem : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void Exit()
+    {
+        Debug.Log("Saliendo del juego....");
+        Application.Quit();
+    }
+
+}
